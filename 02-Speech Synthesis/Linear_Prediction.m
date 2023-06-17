6a2723d26ae71587d3999fbfa40f3199dd52a3f3
es-OmarHani <es-OmarHani2024@alexu.edu.eg>
2023-06-17 14:28:23 +0300

    fix: Final Version

--- 02-Speech Synthesis/Linear_Prediction.m
+++ 02-Speech Synthesis/Linear_Prediction.m
@@ -1,129 +1,9 @@
-% %% Code 1 Of speech Synthsis with Linear Filter
+%% Code 1 Of speech Synthsis with Linear Filter
 
-% Clear all
-clc ; clear all ; close all ;
- 
-% % Load speech signal
-% [y, Fs] = audioread('Recording (8).m4a');
-
-% % Segment length in seconds
-% seg_len_sec = 0.015;
- 
-% % LPC order
-% p = 10;
- 
-% % Compute segment length in samples
-% seg_len = round(seg_len_sec * Fs);
- 
-% % Number of segments
-% num_seg = floor(length(y) / seg_len);
- 
-% % Initialize synthesized speech signal
-% y_syn = zeros(length(y));
- 
- 
-% % Loop through each segment
-% for i = 1:seg_len:length(y)-seg_len
-%     % Extract current segment
-%     seg = y((i-1)*seg_len+1:i*seg_len);
-%     % Compute LPC coefficients
-%     a = lpc(seg, p);
-%     if i == 1 
-%         %Initial value for segments
-%         zi = zeros(max(length(a),length(1))-1) ;
-%     end
-%     % Get Estimation Value Fm
-%     [zf , est_val] = filter(a,1,seg);
-%     % Make Initial for next seg is final from current seg 
-%     zi = zf ;
-%     % Synthesize speech using LPC coefficients
-%     seg_syn = filter(1, a , seg);
-%     % Add synthesized segment to output signal
-%     y_syn((i-1)*seg_len+1:i*seg_len) = seg_syn;
-% end
-
- 
-% figure
-% % Plot the first set of data in the first subplot
-% subplot(1,2,1)
-% plot(y)
-% title('Plot 1')
-
-% % Plot the second set of data in the second subplot
-% subplot(1,2,2)
-% plot(y_syn)
-% title('Plot 2')
-% Load speech signal
-[y, Fs] = audioread('Recording (8).m4a');
-
-% Segment length in seconds
-seg_len_sec = 0.015;
-
-% LPC order
-p = 10;
-
-% Compute segment length in samples
-seg_len = round(seg_len_sec * Fs);
-
-% Number of segments
-num_seg = floor(length(y) / seg_len);
-
-% Initialize synthesized speech signal
-y_syn = zeros(size(y));
-
-% 
-zi_1 = 0 ;
-zi_2 = 0 ;
-
-seg_analysis_vec = zeros(num_seg,1);
-a_vec = zeros(num_seg,1);
-
-% Loop through each segment
-for i = 1:seg_len:length(y)-seg_len
-    
-    % Extract current segment
-    seg = y((i-1)*seg_len+1:i*seg_len);
-    % Compute LPC coefficients
-    a = lpc(seg, p);
-    %analysis speech using LPC coefficients 
-    [seg_analysis , zf_1] = filter(a, 1, seg , zi_1);
-    % Save zi
-    zi_1 = zf_1 ;
-    
-    % Save Vectors
-    seg_analysis_vec(i) = seg_analysis;
-    a_vec(i) = a;
-     
-end
-
-
-
-    % Synthesize speech using LPC coefficients
-    [seg_syn , zf_2] = filter(1 , a , seg_analysis);
-    x = length(seg_syn);
-    y = length(y_syn((i-1)*seg_len+1:i*seg_len));
-    Add synthesized segment to output signal
-    y_syn((i-1)*seg_len+1:i*seg_len) = seg_syn;   
-
-figure
-% Plot the first set of data in the first subplot
-subplot(1,2,1)
-plot(y)
-title('Plot 1')
-
-% Plot the second set of data in the second subplot
-subplot(1,2,2)
-plot(y_syn)
-title('Plot 2')
-% Write synthesized speech to file
-% audiowrite('speech_synthesized.wav', y_syn, Fs);
-
-%% Code 2 Of speech Synthsis with Linear Filter
-
-clc ; clear all ; close all ;
+clc ; clear; close all ;
 
 % Load speech signal
-[y, Fs] = audioread('Recording (8).m4a');
+[y, Fs] = audioread('Recording (24).m4a');
 
 % Segment length in seconds
 seg_len_sec = 0.015;
@@ -175,7 +55,75 @@ ylabel('Amplitude');
 % 
 % subplot(3,1,3);
 % plot(a);
-% title('LPC Paramters');
+title('LPC Paramters');
+
+%% Code 2 Of speech Synthsis with Linear Filter
+clc; 
+clear;
+close all;
+
+% Load speech signal
+[y, Fs] = audioread('Recording (24).m4a');
+
+% Segment length in seconds
+seg_len_sec = 0.015;
+
+% LPC order
+p = 10;
+
+% Compute segment length in samples
+seg_len = round(seg_len_sec * Fs);
+
+% Compute the number of segments
+num_segments = floor(length(y) / seg_len);
+
+% Initialize synthesized speech
+synthesized = zeros(size(y));
+
+% Run sound before synthesized segments
+sound(y, Fs);
+% Pause for a short duration between segments
+pause(8);
+
+for i = 1:num_segments
+    % Extract segment
+    seg_start = (i - 1) * seg_len + 1;
+    seg_end = i * seg_len;
+    seg = y(seg_start:seg_end);
+
+    % Compute LPC coefficients
+    a = lpc(seg, p);
+
+    % Get Estimation Value Fm
+    est_x = filter(a, 1, seg);
+    [acs, lags] = xcorr(est_x, 'coeff');
+    
+    % Synthesize speech using LPC coefficients
+    seg_syn = filter(1, a, est_x);
+    
+    % Update synthesized speech with the current segment
+    synthesized(seg_start:seg_end) = seg_syn;
+end
+
+% Plot original and synthesized speech for the entire record
+t = (0:length(y)-1) / Fs;
+
+figure;
+subplot(2, 1, 1);
+plot(t, y);
+title('Original Speech');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+subplot(2, 1, 2);
+plot(t, synthesized);
+title('Synthesized Speech');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+% Run sound for the synthesized speech
+sound(synthesized, Fs);
+
 
 
 
