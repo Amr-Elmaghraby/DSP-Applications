fa38b518f6b25cc4b243258c3ddd8ac3fcfdf549
es-OmarHani <es-OmarHani2024@alexu.edu.eg>
2023-06-17 16:33:36 +0300

    fix: mat only with voice

--- Final Project Speech Analysis/stabilizeLPC.m
+++ Final Project Speech Analysis/stabilizeLPC.m
@@ -7,22 +7,10 @@ function lpcCoefficients = stabilizeLPC(lpcCoefficients)
 % Output:
 %   - lpcCoefficients: The stabilized LPC coefficients.
 
-    % Ensure LPC coefficients are within the unit circle
-    lpcCoefficients = lpcCoefficients / max(abs(lpcCoefficients));
-    
-    % Check if any coefficients have magnitude >= 1
-    unstableIndices = find(abs(lpcCoefficients) >= 1);
-    if ~isempty(unstableIndices)
-        % Apply a scaling factor to bring the coefficients inside the unit circle
-        lpcCoefficients(unstableIndices) = lpcCoefficients(unstableIndices) / 1.01;
+    % Check if any coefficients lie outside the unit circle
+    if any(abs(roots(lpcCoefficients)) >= 1)
+        % Normalize the coefficients to ensure they lie inside the unit circle
+        lpcCoefficients = lpcCoefficients / max(abs(roots(lpcCoefficients)));
     end
-    
-    % Adjust coefficients to satisfy the stability condition
-    poles = roots(lpcCoefficients);
-    unstablePoles = abs(poles) >= 1;
-    poles(unstablePoles) = poles(unstablePoles) ./ abs(poles(unstablePoles));
-    
-    % Reconstruct the stabilized LPC coefficients
-    lpcCoefficients = real(poly(poles));
 
 end
